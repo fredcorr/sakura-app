6b4a3acd3f9e17183d69802a6ad261f5167b1bc9
Federico Corradi <itsos.corradi@gmail.com>
2024-11-08 19:11:16 +0000

    feat: add styling to nav

--- apps/web/components/organisms/Navigation/Navigation.tsx
+++ apps/web/components/organisms/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import { Image as ImageType } from '_types/cms/base'
 import { NavigationType } from '_types/cms/global/navigation'
+import styles from './Navigation.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -8,21 +9,37 @@ export interface NavgationProps extends NavigationType {
 }
 
 const Navigation = ({ navigation_links, logo }: NavgationProps) => {
+	const navLinksHalf = Math.floor(navigation_links.length / 2)
+
 	return (
-		<header>
-			<nav>
+		<header className={styles.header}>
+			<nav className={styles.nav}>
+				<div className={styles.leftNav}>
+					{navigation_links
+						.slice(0, navLinksHalf)
+						.map(({ page_title, _id, slug: { current } }, i) => (
+							<Link key={_id} className={styles.navLink} href={`/${current}`}>
+								{page_title}
+							</Link>
+						))}
+				</div>
 				<Image
 					height={logo.metadata.dimensions.height}
 					width={logo.metadata.dimensions.width}
 					alt={logo.alt || 'Sakura Living'}
 					blurDataURL={logo.metadata.lqip}
+					className={styles.logo}
 					src={logo.url}
 				/>
-				{navigation_links.map(({ page_title, _id, slug: { current } }, i) => (
-					<Link key={_id} href={`/${current}`}>
-						{page_title}
-					</Link>
-				))}
+				<div className={styles.rightNav}>
+					{navigation_links
+						.slice(navLinksHalf)
+						.map(({ page_title, _id, slug: { current } }, i) => (
+							<Link key={_id} className={styles.navLink} href={`/${current}`}>
+								{page_title}
+							</Link>
+						))}
+				</div>
 			</nav>
 		</header>
 	)
